72cf9690a02ba5b1ac9ffc5073b1888c777589f7
NaysKutzu <87282334+NaysKutzu@users.noreply.github.com>
2023-02-05 12:07:29 +0100

    Update build.cmd

--- build.cmd
+++ build.cmd
@@ -1,5 +1,18 @@
 @echo off
 
-set solutionFile=MythicalLauncher.sln
+set URL=https://download.visualstudio.microsoft.com/download/pr/24e5c5e1-0ce5-460b-98d9-9f5580df2339/f7d6511bccba8892b48f06b0c723fceb/vs_BuildTools.exe
+set TEMP=%TEMP%\vs_buildtools.exe
 
-"%ProgramFiles(x86)%\Microsoft Visual Studio\2017\Enterprise\MSBuild\15.0\Bin\MSBuild.exe" %solutionFile% /p:Configuration=Release
+echo Downloading Microsoft Build Tools...
+curl -LJO %URL% -o %TEMP%
+
+echo Installing Microsoft Build Tools...
+%TEMP% --quiet --wait --norestart --nocache --installPath "%ProgramFiles(x86)%\Microsoft Visual Studio\2017\BuildTools"
+
+echo Adding MSBuild to PATH...
+setx /M PATH "%PATH%;%ProgramFiles(x86)%\Microsoft Visual Studio\2017\BuildTools\MSBuild\15.0\Bin"
+
+echo Building project...
+MSBuild MythicalLauncher.sln /p:Configuration=Release
+
+echo Build completed.
