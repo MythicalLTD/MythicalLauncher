bc675ef8668bb587cb729fdaac1b4eb200c0a437
NaysKutzu <87282334+NaysKutzu@users.noreply.github.com>
2023-02-05 12:09:28 +0100

    Update build.cmd

--- build.cmd
+++ build.cmd
@@ -1,18 +1,15 @@
 @echo off
 
-set URL=https://download.visualstudio.microsoft.com/download/pr/24e5c5e1-0ce5-460b-98d9-9f5580df2339/f7d6511bccba8892b48f06b0c723fceb/vs_BuildTools.exe
-set TEMP=%TEMP%\vs_buildtools.exe
-
 echo Downloading Microsoft Build Tools...
-curl -LJO %URL% -o %TEMP%
+powershell -Command "Invoke-WebRequest -Uri 'https://aka.ms/vs/16/release/vs_buildtools.exe' -OutFile 'vs_buildtools.exe'"
 
 echo Installing Microsoft Build Tools...
-%TEMP% --quiet --wait --norestart --nocache --installPath "%ProgramFiles(x86)%\Microsoft Visual Studio\2017\BuildTools"
+vs_buildtools.exe --quiet --wait --norestart --nocache --installPath "C:\BuildTools"
 
 echo Adding MSBuild to PATH...
-setx /M PATH "%PATH%;%ProgramFiles(x86)%\Microsoft Visual Studio\2017\BuildTools\MSBuild\15.0\Bin"
+setx PATH "%PATH%;C:\BuildTools\MSBuild\Current\Bin"
 
 echo Building project...
-MSBuild MythicalLauncher.sln /p:Configuration=Release
+msbuild MythicalLauncher.sln /p:Configuration=Release
 
 echo Build completed.
