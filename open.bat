68f99614ce201cf8c12c43ac6b063841c9570f70
MythicalDoggo <87282334+NaysKutzu@users.noreply.github.com>
2023-02-14 20:39:53 +0100

    Fix

--- open.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-set outputDirectory="C:\Users\MythicalDoggo\Desktop\MythicalLauncher Debug"
-cd %outputDirectory%
-del /Q /F *.xml
-del /Q /F *.dll
-del /Q /F *.exe.config
-del /Q /F *.pdb
-start MythicalLauncher.exe 
-exit
